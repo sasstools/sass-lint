9050ea9556f20e80c2479e2465a97dfb153c14d2
Sam Richard <snugug@gmail.com>
2015-08-30 13:20:12 -0400

    Add working CLI code
    So, will lint from the CLI now it seems

--- bin/sass-lint.js
+++ bin/sass-lint.js
@@ -2,17 +2,23 @@
 'use strict';
 
 var program = require('commander'),
-    meta = require('../package.json');
+    meta = require('../package.json'),
+    lint = require('../index');
 
-var exitCode = 0;
-    // lint = require('../index');
+var detects;
 
 program
   .version(meta.version)
+  .usage('[options] \'<file or glob>\'')
+  .option('-q, --no-exit', 'do not exit on errors')
   .parse(process.argv);
 
-// if (program.version) {
-//   console.log('v' + program.version);
-// }
 
-// console.log(userArgs);
+detects = lint.lintFiles(program.args[0]);
+detects = lint.format(detects);
+
+lint.outputResults(detects);
+
+if (program.exit) {
+  lint.failOnError(detects);
+}
