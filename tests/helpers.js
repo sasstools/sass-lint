31e9efb0908c22ea03ebacfbc92a7adba79d7864
Dan Purdy <danjpurdy@gmail.com>
2015-09-06 02:31:04 +0100

    :white_check_mark: Add tests for helper methods #116

---
+++ tests/helpers.js
@@ -0,0 +1,234 @@
+'use strict';
+
+var assert = require('assert'),
+    helpers = require('../lib/helpers');
+
+describe('helpers', function () {
+
+  //////////////////////////////
+  // Helpers is loaded
+  //////////////////////////////
+  it('helpers should not be undefined', function (done) {
+
+    var loaded = typeof (helpers) === 'object' ? true : false;
+
+    assert.equal(true, loaded);
+    done();
+  });
+
+  //////////////////////////////
+  // Is Number
+  //////////////////////////////
+
+  it('isNumber - [10 - true]', function (done) {
+
+    var result = helpers.isNumber(10);
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isNumber - [\'10\' - true]', function (done) {
+
+    var result = helpers.isNumber('10');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isNumber - [\'ten\' - false]', function (done) {
+
+    var result = helpers.isNumber('ten');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isNumber - [\'ff00ff\' - false]', function (done) {
+
+    var result = helpers.isNumber('ff00ff');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  //////////////////////////////
+  // Is Upper Case
+  //////////////////////////////
+
+  it('isUpperCase - [\'TEST\' - true]', function (done) {
+
+    var result = helpers.isUpperCase('TEST');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isUpperCase - [\'test\' - false]', function (done) {
+
+    var result = helpers.isUpperCase('test');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isUpperCase - [abcDEF - false]', function (done) {
+
+    var result = helpers.isUpperCase('abcDEF');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isUpperCase - [\'123\' - false]', function (done) {
+
+    var result = helpers.isUpperCase('123');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  //////////////////////////////
+  // Is Lower Case
+  //////////////////////////////
+
+  it('isLowerCase - [\'TEST\' - false]', function (done) {
+
+    var result = helpers.isLowerCase('TEST');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isLowerCase - [\'test\' - true]', function (done) {
+
+    var result = helpers.isLowerCase('test');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isLowerCase - [abcDEF - false]', function (done) {
+
+    var result = helpers.isLowerCase('abcDEF');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isLowerCase - [\'123\' - false]', function (done) {
+
+    var result = helpers.isLowerCase('123');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  //////////////////////////////
+  // Is Valid Hex
+  //////////////////////////////
+
+  it('isValidHex - [\'abcdef\' - true]', function (done) {
+
+    var result = helpers.isValidHex('abcdef');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'ABCDEF\' - true]', function (done) {
+
+    var result = helpers.isValidHex('ABCDEF');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'123456\' - true]', function (done) {
+
+    var result = helpers.isValidHex('123456');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'ABC123\' - true]', function (done) {
+
+    var result = helpers.isValidHex('ABC123');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'abc123\' - true]', function (done) {
+
+    var result = helpers.isValidHex('abc123');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'fff\' - true]', function (done) {
+
+    var result = helpers.isValidHex('fff');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'123\' - true]', function (done) {
+
+    var result = helpers.isValidHex('123');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isValidHex - [\'efghij\' - false]', function (done) {
+
+    var result = helpers.isValidHex('efghij');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isValidHex - [\'thj\' - false]', function (done) {
+
+    var result = helpers.isValidHex('thj');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isValidHex - [\'ab\' - false]', function (done) {
+
+    var result = helpers.isValidHex('ab');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isValidHex - [\'12\' - false]', function (done) {
+
+    var result = helpers.isValidHex('12');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isValidHex - [\'1234567\' - false]', function (done) {
+
+    var result = helpers.isValidHex('1234567');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isValidHex - [\'abc1234\' - false]', function (done) {
+
+    var result = helpers.isValidHex('abc1234');
+
+    assert.equal(false, result);
+    done();
+  });
+});
