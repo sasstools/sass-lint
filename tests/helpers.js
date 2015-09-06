1ea5e269f8865eca096b939c573ac775abd7f39b
Dan Purdy <danjpurdy@gmail.com>
2015-09-06 11:10:38 +0100

    :white_check_mark: Add tests for isUnique #116

--- tests/helpers.js
+++ tests/helpers.js
@@ -32,6 +32,26 @@ var classBlock =
       start: { line: 5, column: 1 },
       end: { line: 5, column: 4 },
       indexHasChanged: [ 0 ]
+    },
+
+    otherClassBlock =
+    {
+      type: 'class',
+      content:
+          [
+              {
+                type: 'ident',
+                content: 'test',
+                syntax: 'scss',
+                start: { line: 9, column: 2 },
+                end: { line: 9, column: 5 },
+                indexHasChanged: [ 0 ]
+              }
+          ],
+      syntax: 'scss',
+      start: { line: 9, column: 1 },
+      end: { line: 9, column: 5 },
+      indexHasChanged: [ 0 ]
     };
 
 
@@ -165,6 +185,34 @@ describe('helpers', function () {
           content: [1, 2, 3, 4]
         },
         result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(false, result);
+    done();
+  });
+
+  //////////////////////////////
+  // isUnique
+  //////////////////////////////
+
+  it('isUnique should return false - same object passed', function (done) {
+
+
+    var ledger = [];
+    ledger.push(classBlock);
+
+    var result = helpers.isUnique(ledger, classBlock);
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isUnique should return true - unique object passed', function (done) {
+
+
+    var ledger = [];
+    ledger.push(classBlock);
+
+    var result = helpers.isUnique(ledger, otherClassBlock);
 
     assert.equal(false, result);
     done();
