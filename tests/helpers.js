792fb50b12c9ea637dbf8ca45acc3d95ef484037
Dan Purdy <danjpurdy@gmail.com>
2015-09-06 03:26:37 +0100

    :white_check_mark: Add tests for isEqual function

--- tests/helpers.js
+++ tests/helpers.js
@@ -14,6 +14,27 @@ var haystack = [
     }
 ];
 
+var classBlock =
+    {
+      type: 'class',
+      content:
+          [
+              {
+                type: 'ident',
+                content: 'foo',
+                syntax: 'scss',
+                start: { line: 5, column: 2 },
+                end: { line: 5, column: 4 },
+                indexHasChanged: [ 0 ]
+              }
+          ],
+      syntax: 'scss',
+      start: { line: 5, column: 1 },
+      end: { line: 5, column: 4 },
+      indexHasChanged: [ 0 ]
+    };
+
+
 describe('helpers', function () {
 
   //////////////////////////////
@@ -59,6 +80,93 @@ describe('helpers', function () {
         result = helpers.propertySearch(haystack, needle, property);
 
     assert.equal(1, result);
+    done();
+  });
+
+  //////////////////////////////
+  // isEqual
+  //////////////////////////////
+
+  it('isEqual should return true', function (done) {
+
+    var testBlock =
+        {
+          type: 'class',
+          content:
+              [
+                  {
+                    type: 'ident',
+                    content: 'foo',
+                    syntax: 'scss',
+                    start: { line: 5, column: 2 },
+                    end: { line: 5, column: 4 },
+                    indexHasChanged: [ 0 ]
+                  }
+              ],
+          syntax: 'scss',
+          start: { line: 5, column: 1 },
+          end: { line: 5, column: 4 },
+          indexHasChanged: [ 0 ]
+        },
+        result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isEqual should return false - incorrect type', function (done) {
+
+    var testBlock = {
+          type: 'block',
+          start: {line: 5},
+          end: {line: 5},
+          content: [1]
+        },
+        result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isEqual should return false - incorrect start line', function (done) {
+
+    var testBlock = {
+          type: 'class',
+          start: {line: 7},
+          end: {line: 5},
+          content: [1]
+        },
+        result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isEqual should return false - incorrect end line', function (done) {
+
+    var testBlock = {
+          type: 'class',
+          start: {line: 5},
+          end: {line: 7},
+          content: [1]
+        },
+        result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isEqual should return false - incorrect content length', function (done) {
+
+    var testBlock = {
+          type: 'class',
+          start: {line: 5},
+          end: {line: 5},
+          content: [1, 2, 3, 4]
+        },
+        result = helpers.isEqual(classBlock, testBlock);
+
+    assert.equal(false, result);
     done();
   });
 
