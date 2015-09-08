fdbf0d315c638f9d1e62a048193afebf3aa6cf96
Ben Griffith <gt11687@gmail.com>
2015-09-08 17:53:32 +0100

    :bug: Fix issue with non-paren statements

--- tests/rules/brace-style.js
+++ tests/rules/brace-style.js
@@ -24,7 +24,7 @@ describe('brace style', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(38, data.warningCount);
+      lint.assert.equal(39, data.warningCount);
       done();
     });
   });
@@ -54,7 +54,7 @@ describe('brace style', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(36, data.warningCount);
+      lint.assert.equal(37, data.warningCount);
       done();
     });
   });
@@ -69,7 +69,7 @@ describe('brace style', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(22, data.warningCount);
+      lint.assert.equal(23, data.warningCount);
       done();
     });
   });
@@ -84,7 +84,7 @@ describe('brace style', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(40, data.warningCount);
+      lint.assert.equal(42, data.warningCount);
       done();
     });
   });
