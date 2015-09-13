493ce1acc0f8364999750ce1ffaa5775368cbaf1
Dan Purdy <danjpurdy@gmail.com>
2015-09-14 00:42:30 +0100

    :white_check_mark: Add shorthand-values rule tests

---
+++ tests/rules/shorthand-values.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var lint = require('./_lint');
+
+var file = lint.file('shorthand-values.scss');
+
+describe('shorthand values', function () {
+  it('[default]', function (done) {
+    lint.test(file, {
+      'shorthand-values': 1
+    }, function (data) {
+      lint.assert.equal(44, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 1]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            1
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(19, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 2]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            2
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(20, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 3]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            3
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(26, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: none]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(0, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 1, 2]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            1,
+            2
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(32, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 1, 3]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            1,
+            3
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(38, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 2, 3]', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            2,
+            3
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(32, data.warningCount);
+      done();
+    });
+  });
+
+  it('[allowed: 1, 2, 3] - as default', function (done) {
+    lint.test(file, {
+      'shorthand-values': [
+        1,
+        {
+          'allowed-shorthands': [
+            1,
+            2,
+            3
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(44, data.warningCount);
+      done();
+    });
+  });
+});
