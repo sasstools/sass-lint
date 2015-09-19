d0f4d99423f2ee97d0e8cff1c7636c3e190db41c
Ben Griffith <gt11687@gmail.com>
2015-09-19 10:58:37 +0100

    :white_check_mark: Add Sass tests for hex rules

--- tests/rules/hex-length.js
+++ tests/rules/hex-length.js
@@ -2,13 +2,13 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('hex-length.scss');
-
 describe('hex length', function () {
   //////////////////////////////
   // Hex Length Short - Default
   //////////////////////////////
-  it('[style: short]', function (done) {
+  it('scss - [style: short]', function (done) {
+    var file = lint.file('hex-length.scss');
+
     lint.test(file, {
       'hex-length': 1
     }, function (data) {
@@ -20,7 +20,42 @@ describe('hex length', function () {
   //////////////////////////////
   // Hex Length Long
   //////////////////////////////
-  it('[style: long]', function (done) {
+  it('scss - [style: long]', function (done) {
+    var file = lint.file('hex-length.scss');
+
+    lint.test(file, {
+      'hex-length': [
+        1,
+        {
+          'style': 'long'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Hex Length Short - Default
+  //////////////////////////////
+  it('sass - [style: short]', function (done) {
+    var file = lint.file('hex-length.sass');
+
+    lint.test(file, {
+      'hex-length': 1
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Hex Length Long
+  //////////////////////////////
+  it('sass - [style: long]', function (done) {
+    var file = lint.file('hex-length.sass');
+
     lint.test(file, {
       'hex-length': [
         1,
--- tests/rules/hex-notation.js
+++ tests/rules/hex-notation.js
@@ -2,13 +2,13 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('hex-notation.scss');
-
 describe('hex notation', function () {
   //////////////////////////////
   // Hex notation Lowercase - Default
   //////////////////////////////
-  it('[style: lowercase]', function (done) {
+  it('scss - [style: lowercase]', function (done) {
+    var file = lint.file('hex-notation.scss');
+
     lint.test(file, {
       'hex-notation': 1
     }, function (data) {
@@ -20,7 +20,42 @@ describe('hex notation', function () {
   //////////////////////////////
   // Hex Notation Uppercase
   //////////////////////////////
-  it('[style: uppercase]', function (done) {
+  it('scss - [style: uppercase]', function (done) {
+    var file = lint.file('hex-notation.scss');
+
+    lint.test(file, {
+      'hex-notation': [
+        1,
+        {
+          'style': 'uppercase'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Hex notation Lowercase - Default
+  //////////////////////////////
+  it('sass - [style: lowercase]', function (done) {
+    var file = lint.file('hex-notation.sass');
+
+    lint.test(file, {
+      'hex-notation': 1
+    }, function (data) {
+      lint.assert.equal(6, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Hex Notation Uppercase
+  //////////////////////////////
+  it('sass - [style: uppercase]', function (done) {
+    var file = lint.file('hex-notation.sass');
+
     lint.test(file, {
       'hex-notation': [
         1,
