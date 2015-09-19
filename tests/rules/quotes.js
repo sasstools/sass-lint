84d6e28fcd676d5d570c05106ff3767ae8ef3468
Ben Griffith <gt11687@gmail.com>
2015-09-19 12:26:53 +0100

    :white_check_mark: Add sass tests for quotes rules

--- tests/rules/quotes.js
+++ tests/rules/quotes.js
@@ -2,9 +2,36 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('quotes.scss');
+describe('quotes - scss', function () {
+  var file = lint.file('quotes.scss');
 
-describe('quotes', function () {
+  it('[style: single]', function (done) {
+    lint.test(file, {
+      'quotes': 1
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+
+  it('[style: double]', function (done) {
+    lint.test(file, {
+      'quotes': [
+        1,
+        {
+          'style': 'double'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('quotes - sass', function () {
+  var file = lint.file('quotes.sass');
+
   it('[style: single]', function (done) {
     lint.test(file, {
       'quotes': 1
--- tests/rules/url-quotes.js
+++ tests/rules/url-quotes.js
@@ -2,14 +2,27 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('url-quotes.scss');
+describe('url quotes - scss', function () {
+  var file = lint.file('url-quotes.scss');
 
-describe('url quotes', function () {
   it('enforce', function (done) {
     lint.test(file, {
       'url-quotes': 1
     }, function (data) {
       lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('url quotes - sass', function () {
+  var file = lint.file('url-quotes.sass');
+
+  it('enforce', function (done) {
+    lint.test(file, {
+      'url-quotes': 1
+    }, function (data) {
+      lint.assert.equal(1, data.warningCount);
       done();
     });
   });
