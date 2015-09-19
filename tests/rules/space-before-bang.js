d5be95f38ed0123f2cfbecd840b874d4e60dec1a
Ben Griffith <gt11687@gmail.com>
2015-09-19 10:41:58 +0100

    :white_check_mark: Add Sass tests for space-before-... rules

--- tests/rules/space-before-bang.js
+++ tests/rules/space-before-bang.js
@@ -2,9 +2,36 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('space-before-bang.scss');
+describe('space before bang - scss', function () {
+  var file = lint.file('space-before-bang.scss');
 
-describe('space before bang', function () {
+  it('[include: true]', function (done) {
+    lint.test(file, {
+      'space-before-bang': 1
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+
+  it('[include: false]', function (done) {
+    lint.test(file, {
+      'space-before-bang': [
+        1,
+        {
+          'include': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('space before bang - sass', function () {
+  var file = lint.file('space-before-bang.sass');
+
   it('[include: true]', function (done) {
     lint.test(file, {
       'space-before-bang': 1
--- tests/rules/space-before-colon.js
+++ tests/rules/space-before-colon.js
@@ -2,9 +2,36 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('space-before-colon.scss');
+describe('space before colon - scss', function () {
+  var file = lint.file('space-before-colon.scss');
 
-describe('space before colon', function () {
+  it('[include: false]', function (done) {
+    lint.test(file, {
+      'space-before-colon': 1
+    }, function (data) {
+      lint.assert.equal(3, data.warningCount);
+      done();
+    });
+  });
+
+  it('[include: true]', function (done) {
+    lint.test(file, {
+      'space-before-colon': [
+        1,
+        {
+          'include': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('space before colon - sass', function () {
+  var file = lint.file('space-before-colon.sass');
+
   it('[include: false]', function (done) {
     lint.test(file, {
       'space-before-colon': 1
