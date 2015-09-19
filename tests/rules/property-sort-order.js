7b21f833a08c2a548ca5b44a24ab9a7e00d92b37
Ben Griffith <gt11687@gmail.com>
2015-09-19 11:23:21 +0100

    :white_check_mark: Sass tests for property-sort-order

--- tests/rules/property-sort-order.js
+++ tests/rules/property-sort-order.js
@@ -2,9 +2,84 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('property-sort-order.scss');
+describe('property sort order - scss', function () {
+  var file = lint.file('property-sort-order.scss');
 
-describe('property sort order', function () {
+  it('[order: alphabetical]', function (done) {
+    lint.test(file, {
+      'property-sort-order': 1
+    }, function (data) {
+      lint.assert.equal(6, data.warningCount);
+      done();
+    });
+  });
+
+  it('[order: custom]', function (done) {
+    lint.test(file, {
+      'property-sort-order': [
+        1,
+        {
+          'order': [
+            'height',
+            'width',
+            'display',
+            'color'
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+
+  it('[order: recess]', function (done) {
+    lint.test(file, {
+      'property-sort-order': [
+        1,
+        {
+          'order': 'recess'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(5, data.warningCount);
+      done();
+    });
+  });
+
+  it('[order: smacss]', function (done) {
+    lint.test(file, {
+      'property-sort-order': [
+        1,
+        {
+          'order': 'smacss'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(5, data.warningCount);
+      done();
+    });
+  });
+
+  it('[order: concentric]', function (done) {
+    lint.test(file, {
+      'property-sort-order': [
+        1,
+        {
+          'order': 'concentric'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+});
+
+
+describe('property sort order - sass', function () {
+  var file = lint.file('property-sort-order.sass');
+
   it('[order: alphabetical]', function (done) {
     lint.test(file, {
       'property-sort-order': 1
