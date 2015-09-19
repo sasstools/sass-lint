4f50c83ce0bea767113c82f562ad9d50e6da4eae
Ben Griffith <gt11687@gmail.com>
2015-09-19 11:03:50 +0100

    :white_check_mark: Add Sass test for other hex rule

--- tests/rules/no-invalid-hex.js
+++ tests/rules/no-invalid-hex.js
@@ -2,9 +2,22 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('no-invalid-hex.scss');
+describe('no invalid hex - scss', function () {
+  var file = lint.file('no-invalid-hex.scss');
 
-describe('no invalid hex', function () {
+  it('enforce', function (done) {
+    lint.test(file, {
+      'no-invalid-hex': 1
+    }, function (data) {
+      lint.assert.equal(16, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('no invalid hex - sass', function () {
+  var file = lint.file('no-invalid-hex.sass');
+
   it('enforce', function (done) {
     lint.test(file, {
       'no-invalid-hex': 1
