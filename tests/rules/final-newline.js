2b8d96b2f5bc3857acc68aacb7966a2fdd35dd2a
Ben Griffith <gt11687@gmail.com>
2015-09-21 17:20:06 +0100

    :white_check_mark: Add Sass tests for final-newline rule

--- tests/rules/final-newline.js
+++ tests/rules/final-newline.js
@@ -2,7 +2,7 @@
 
 var lint = require('./_lint');
 
-describe('final newline', function () {
+describe('final newline - scss', function () {
   //////////////////////////////
   // Final Newline w/Return
   //////////////////////////////
@@ -36,6 +36,50 @@ describe('final newline', function () {
   //////////////////////////////
   it('no space', function (done) {
     var file = lint.file('final-newline.scss');
+
+    lint.test(file, {
+      'final-newline': 1
+    }, function (data) {
+      lint.assert.equal(1, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('final newline - sass', function () {
+  //////////////////////////////
+  // Final Newline w/Return
+  //////////////////////////////
+  it('with return', function (done) {
+    var file = lint.file('final-newline--return.sass');
+
+    lint.test(file, {
+      'final-newline': 1
+    }, function (data) {
+      lint.assert.equal(0, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Final Newline w/space
+  //////////////////////////////
+  it('with space', function (done) {
+    var file = lint.file('final-newline--space.sass');
+
+    lint.test(file, {
+      'final-newline': 1
+    }, function (data) {
+      lint.assert.equal(1, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Final Newline
+  //////////////////////////////
+  it('no space', function (done) {
+    var file = lint.file('final-newline.sass');
 
     lint.test(file, {
       'final-newline': 1
