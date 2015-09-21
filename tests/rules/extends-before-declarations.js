e290c31d91e9f4f1deb2aaadebe66e2393e1f4ef
Ben Griffith <gt11687@gmail.com>
2015-09-21 12:38:57 +0100

    :white_check_mark: Add Sass tests for extends-before-declarations, extends-before-mixins rules

--- tests/rules/extends-before-declarations.js
+++ tests/rules/extends-before-declarations.js
@@ -2,12 +2,22 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('extends-before-declarations.scss');
+describe('extends before declarations - scss', function () {
+  var file = lint.file('extends-before-declarations.scss');
 
-describe('extends before declarations', function () {
-  //////////////////////////////
-  // Extends Before Declaration
-  //////////////////////////////
+  it('enforce', function (done) {
+    lint.test(file, {
+      'extends-before-declarations': 1
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('extends before declarations - sass', function () {
+  var file = lint.file('extends-before-declarations.sass');
+
   it('enforce', function (done) {
     lint.test(file, {
       'extends-before-declarations': 1
--- tests/rules/extends-before-mixins.js
+++ tests/rules/extends-before-mixins.js
@@ -2,9 +2,22 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('extends-before-mixins.scss');
+describe('extends before mixins - scss', function () {
+  var file = lint.file('extends-before-mixins.scss');
 
-describe('extends before mixins', function () {
+  it('enforce', function (done) {
+    lint.test(file, {
+      'extends-before-mixins': 1
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('extends before mixins - sass', function () {
+  var file = lint.file('extends-before-mixins.sass');
+
   it('enforce', function (done) {
     lint.test(file, {
       'extends-before-mixins': 1
