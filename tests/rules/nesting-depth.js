305ee7947734a5da2f5f66921da8d5a2f96266f3
Ben Griffith <gt11687@gmail.com>
2015-09-21 13:28:27 +0100

    :white_check_mark: Add Sass test for nesting-depth

--- tests/rules/nesting-depth.js
+++ tests/rules/nesting-depth.js
@@ -2,12 +2,22 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('nesting-depth.scss');
+describe('nesting depth - scss', function () {
+  var file = lint.file('nesting-depth.scss');
 
-describe('nesting depth', function () {
-  //////////////////////////////
-  // Nesting Depth
-  //////////////////////////////
+  it('[max-depth: 2]', function (done) {
+    lint.test(file, {
+      'nesting-depth': 1
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('nesting depth - sass', function () {
+  var file = lint.file('nesting-depth.sass');
+
   it('[max-depth: 2]', function (done) {
     lint.test(file, {
       'nesting-depth': 1
