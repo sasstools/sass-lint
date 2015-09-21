df640ee71b5bc7061c27cb96d5bfd3634c9e3ab6
Ben Griffith <gt11687@gmail.com>
2015-09-21 17:24:04 +0100

    Uncomment accidentally commented tests for clean-import-paths

--- tests/rules/clean-import-paths.js
+++ tests/rules/clean-import-paths.js
@@ -13,49 +13,49 @@ describe('clean import paths - scss', function () {
       done();
     });
   });
-  //
-  // it('[leading-underscore: true, filename-extension: false]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'leading-underscore': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
-  //
-  // it('[leading-underscore: false, filename-extension: true]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'filename-extension': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
-  //
-  // it('[leading-underscore: true, filename-extension: true]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'leading-underscore': true,
-  //         'filename-extension': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
+
+  it('[leading-underscore: true, filename-extension: false]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'leading-underscore': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+
+  it('[leading-underscore: false, filename-extension: true]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'filename-extension': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+
+  it('[leading-underscore: true, filename-extension: true]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'leading-underscore': true,
+          'filename-extension': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
 });
 
 
@@ -71,47 +71,47 @@ describe('clean import paths - sass', function () {
       done();
     });
   });
-  //
-  // it('[leading-underscore: true, filename-extension: false]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'leading-underscore': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
-  //
-  // it('[leading-underscore: false, filename-extension: true]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'filename-extension': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
-  //
-  // it('[leading-underscore: true, filename-extension: true]', function (done) {
-  //   lint.test(file, {
-  //     'clean-import-paths': [
-  //       1,
-  //       {
-  //         'leading-underscore': true,
-  //         'filename-extension': true
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(8, data.warningCount);
-  //     done();
-  //   });
-  // });
+
+  it('[leading-underscore: true, filename-extension: false]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'leading-underscore': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+
+  it('[leading-underscore: false, filename-extension: true]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'filename-extension': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+
+  it('[leading-underscore: true, filename-extension: true]', function (done) {
+    lint.test(file, {
+      'clean-import-paths': [
+        1,
+        {
+          'leading-underscore': true,
+          'filename-extension': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
 });
