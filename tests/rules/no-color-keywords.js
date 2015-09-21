11028c129fcc75cfd72230bd3729467d5dd2d9e7
Ben Griffith <gt11687@gmail.com>
2015-09-21 12:44:31 +0100

    :white_check_mark: Add Sass tests for no-color-... disallows

--- tests/rules/no-color-keywords.js
+++ tests/rules/no-color-keywords.js
@@ -2,13 +2,23 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('no-color-keywords.scss');
-
-describe('no color keywords', function () {
-  //////////////////////////////
-  // Color Keyword
-  //////////////////////////////
+describe('no color keywords - scss', function () {
   it('enforce', function (done) {
+    var file = lint.file('no-color-keywords.scss');
+
+    lint.test(file, {
+      'no-color-keywords': 1
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('no color keywords - sass', function () {
+  it('enforce', function (done) {
+    var file = lint.file('no-color-keywords.sass');
+
     lint.test(file, {
       'no-color-keywords': 1
     }, function (data) {
--- tests/rules/no-color-literals.js
+++ tests/rules/no-color-literals.js
@@ -2,12 +2,9 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('no-color-literals.scss');
+describe('no color literals - scss', function () {
+  var file = lint.file('no-color-literals.scss');
 
-describe('no color literals', function () {
-  //////////////////////////////
-  // Color Variable
-  //////////////////////////////
   it('[allow-rgba: false]', function (done) {
     lint.test(file, {
       'no-color-literals': 1
@@ -17,9 +14,34 @@ describe('no color literals', function () {
     });
   });
 
-  //////////////////////////////
-  // Color Variable - allow rgba
-  //////////////////////////////
+  it('[allow-rgba: true]', function (done) {
+    lint.test(file, {
+      'no-color-literals': [
+        1,
+        {
+          'allow-rgba': true
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(18, data.warningCount);
+      done();
+    });
+  });
+});
+
+
+describe('no color literals - sass', function () {
+  var file = lint.file('no-color-literals.sass');
+
+  it('[allow-rgba: false]', function (done) {
+    lint.test(file, {
+      'no-color-literals': 1
+    }, function (data) {
+      lint.assert.equal(19, data.warningCount);
+      done();
+    });
+  });
+
   it('[allow-rgba: true]', function (done) {
     lint.test(file, {
       'no-color-literals': [
