5a55b8d09f89065a62a45379702d6cb957306a86
Ben Griffith <gt11687@gmail.com>
2015-09-22 17:50:30 +0100

    :white_check_mark: Add Sass test for variable-for-property rule

--- tests/rules/variable-for-property.js
+++ tests/rules/variable-for-property.js
@@ -2,9 +2,9 @@
 
 var lint = require('./_lint');
 
-var file = lint.file('variable-for-property.scss');
+describe('variable for property - scss', function () {
+  var file = lint.file('variable-for-property.scss');
 
-describe('variable for property', function () {
   it('[]', function (done) {
     lint.test(file, {
       'variable-for-property': 1
@@ -13,6 +13,37 @@ describe('variable for property', function () {
       done();
     });
   });
+
+  it('[properties]', function (done) {
+    lint.test(file, {
+      'variable-for-property': [
+        1,
+        {
+          'properties': [
+            'margin',
+            'content'
+          ]
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(4, data.warningCount);
+      done();
+    });
+  });
+});
+
+describe('variable for property - sass', function () {
+  var file = lint.file('variable-for-property.sass');
+
+  it('[]', function (done) {
+    lint.test(file, {
+      'variable-for-property': 1
+    }, function (data) {
+      lint.assert.equal(0, data.warningCount);
+      done();
+    });
+  });
+
   it('[properties]', function (done) {
     lint.test(file, {
       'variable-for-property': [
