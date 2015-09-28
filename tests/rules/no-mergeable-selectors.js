c69687bb5a8e69b5b13d0eeb2f173297e6dbe065
Dan Purdy <danjpurdy@gmail.com>
2015-09-28 01:17:37 +0100

    WIP Add rule mergeable selectors

--- tests/rules/no-mergeable-selectors.js
+++ tests/rules/no-mergeable-selectors.js
@@ -14,44 +14,44 @@ describe('no mergeable selectors - scss', function () {
     });
   });
 
-  it('[force-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(2, data.warningCount);
-      done();
-    });
-  });
-});
-
-describe('no mergeable selectors', function () {
-  var file = lint.file('no-mergeable-selectors.sass');
-
-  it('[force-nesting: true]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': 1
-    }, function (data) {
-      lint.assert.equal(3, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-nesting: true]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(2, data.warningCount);
-      done();
-    });
-  });
+//   it('[force-nesting: false]', function (done) {
+//     lint.test(file, {
+//       'no-mergeable-selectors': [
+//         1,
+//         {
+//           'force-nesting': false
+//         }
+//       ]
+//     }, function (data) {
+//       lint.assert.equal(2, data.warningCount);
+//       done();
+//     });
+//   });
+// });
+//
+// describe('no mergeable selectors', function () {
+//   var file = lint.file('no-mergeable-selectors.sass');
+//
+//   it('[force-nesting: true]', function (done) {
+//     lint.test(file, {
+//       'no-mergeable-selectors': 1
+//     }, function (data) {
+//       lint.assert.equal(3, data.warningCount);
+//       done();
+//     });
+//   });
+//
+//   it('[force-nesting: true]', function (done) {
+//     lint.test(file, {
+//       'no-mergeable-selectors': [
+//         1,
+//         {
+//           'force-nesting': false
+//         }
+//       ]
+//     }, function (data) {
+//       lint.assert.equal(2, data.warningCount);
+//       done();
+//     });
+//   });
 });
