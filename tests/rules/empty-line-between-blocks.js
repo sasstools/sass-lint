ada06b30c8d4c34fa1cdff2279fd2c92bac39b6b
Ben Griffith <gt11687@gmail.com>
2015-10-04 21:12:23 +0100

    :white_check_mark: Add non-default tests for empty-line-between-block rules

--- tests/rules/empty-line-between-blocks.js
+++ tests/rules/empty-line-between-blocks.js
@@ -2,12 +2,11 @@
 
 var lint = require('./_lint');
 
-
-describe('empty line between blocks', function () {
-  //////////////////////////////
-  // Empty Line Between Blocks
-  //////////////////////////////
-  it('without comments', function (done) {
+//////////////////////////////
+// SCSS syntax tests
+//////////////////////////////
+describe('empty line between blocks - scss', function () {
+  it('without comments - [include: true]', function (done) {
     var file = lint.file('empty-line-between-blocks.scss');
 
     lint.test(file, {
@@ -18,16 +17,104 @@ describe('empty line between blocks', function () {
     });
   });
 
-  //////////////////////////////
-  // Empty Line With Comment
-  //////////////////////////////
-  it('with comments', function (done) {
+  it('without comments - [include: false]', function (done) {
+    var file = lint.file('empty-line-between-blocks.scss');
+
+    lint.test(file, {
+      'empty-line-between-blocks': [
+        1,
+        {
+          'include': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+
+  it('with comments - [include: true]', function (done) {
     var file = lint.file('empty-line-with-comments.scss');
 
     lint.test(file, {
       'empty-line-between-blocks': 1
     }, function (data) {
       lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+
+  it('with comments - [include: false]', function (done) {
+    var file = lint.file('empty-line-with-comments.scss');
+
+    lint.test(file, {
+      'empty-line-between-blocks': [
+        1,
+        {
+          'include': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(2, data.warningCount);
+      done();
+    });
+  });
+});
+
+//////////////////////////////
+// Sass syntax tests
+//////////////////////////////
+describe('empty line between blocks - sass', function () {
+  it('without comments - [include: true]', function (done) {
+    var file = lint.file('empty-line-between-blocks.sass');
+
+    lint.test(file, {
+      'empty-line-between-blocks': 1
+    }, function (data) {
+      lint.assert.equal(5, data.warningCount);
+      done();
+    });
+  });
+
+  it('without comments - [include: false]', function (done) {
+    var file = lint.file('empty-line-between-blocks.sass');
+
+    lint.test(file, {
+      'empty-line-between-blocks': [
+        1,
+        {
+          'include': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(10, data.warningCount);
+      done();
+    });
+  });
+
+  it('with comments - [include: true]', function (done) {
+    var file = lint.file('empty-line-with-comments.sass');
+
+    lint.test(file, {
+      'empty-line-between-blocks': 1
+    }, function (data) {
+      lint.assert.equal(6, data.warningCount);
+      done();
+    });
+  });
+
+  it('with comments - [include: false]', function (done) {
+    var file = lint.file('empty-line-with-comments.sass');
+
+    lint.test(file, {
+      'empty-line-between-blocks': [
+        1,
+        {
+          'include': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(10, data.warningCount);
       done();
     });
   });
