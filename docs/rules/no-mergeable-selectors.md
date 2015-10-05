b15469de320a0340f17441fd611fc0f505b65889
Dan Purdy <danjpurdy@gmail.com>
2015-10-05 21:49:22 +0100

    :art: Split up mergeable selector rule

--- docs/rules/no-mergeable-selectors.md
+++ docs/rules/no-mergeable-selectors.md
@@ -1,14 +1,9 @@
 # No Mergeable Selectors
 
-Rule `no-mergeable-selectors` will enforce that selectors aren't repeated and that their properties are merged. With the options provided it can also
-enforce the nesting of selectors, pseudo elements/classes and attributes. You may also pass a whitelist of selectors you wish to exclude from merging
-though this whitelist will not affect the warnings surrounding nesting.
+Rule `no-mergeable-selectors` will enforce that selectors aren't repeated and that their properties are merged. You may also pass a whitelist of selectors you wish to exclude from merging.
 
 ## Options
 
-* `force-element-nesting`: `true`/`false` (defaults to `true`)
-* `force-attribute-nesting`: `true`/`false` (defaults to `true`)
-* `force-pseudo-nesting`: `true`/`false` (defaults to `true`)
 * `whitelist`: `[array of selectors]` (defaults to empty array `[]`)
 
 ## Examples
@@ -47,56 +42,6 @@ h1, h2, h3 {
   .bar {
     color: red;
   }
-}
-```
-
-When `force-element-nesting: true`, the following are disallowed. When `force-element-nesting: false`, the following are allowed:
-
-```scss
-// .bar is a nestable selector
-.foo .bar {
-  content: "bar";
-}
-
-// h2 is a nestable selector
-div h2 {
-  color: red;
-}
-
-// .baz is a nestable selector
-.foo {
-  .bar .baz {
-    content: '';
-  }
-}
-```
-
-When `force-attribute-nesting: true`, the following are disallowed. When `force-attribute-nesting: false`, the following are allowed:
-
-```scss
-// the attribute [type="text"] should be nested with a parent selector '&[type="text"]'
-input[type="text"] {
-  content: "bar";
-}
-```
-
-When `force-pseudo-nesting: true`, the following are disallowed. When `force-pseudo-nesting: false`, the following are allowed:
-
-```scss
-// the pseudo element ::first-line should be nested with a parent selector '&::first-line'
-p::first-line {
-    color: #ff0000;
-    font-variant: small-caps;
-}
-
-// the pseudo class `:read-only` should be nested with a parent selector `&:read-only`
-input:read-only {
-  content: '';
-}
-
-// the pseudo class `:nth-of-type(2)` should be nested with a parent selector `&:nth-of-type(2)`
-p:nth-of-type(2) {
-  margin: 0;
 }
 ```
 
--- tests/rules/no-mergeable-selectors.js
+++ tests/rules/no-mergeable-selectors.js
@@ -5,67 +5,9 @@ var lint = require('./_lint');
 describe('no mergeable selectors - scss', function () {
   var file = lint.file('no-mergeable-selectors.scss');
 
-  it('[force-nesting: true]', function (done) {
+  it('[default]', function (done) {
     lint.test(file, {
       'no-mergeable-selectors': 1
-    }, function (data) {
-      lint.assert.equal(36, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-element-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-element-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(30, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-attribute-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-attribute-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(33, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-pseudo-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-pseudo-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(28, data.warningCount);
-      done();
-    });
-  });
-
-  it('[disable all nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-element-nesting': false,
-          'force-attribute-nesting': false,
-          'force-pseudo-nesting': false
-        }
-      ]
     }, function (data) {
       lint.assert.equal(19, data.warningCount);
       done();
@@ -83,76 +25,19 @@ describe('no mergeable selectors - scss', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(35, data.warningCount);
+      lint.assert.equal(18, data.warningCount);
       done();
     });
   });
+
 });
 
 describe('no mergeable selectors - sass', function () {
   var file = lint.file('no-mergeable-selectors.sass');
 
-  it('[force-nesting: true]', function (done) {
+  it('[default]', function (done) {
     lint.test(file, {
       'no-mergeable-selectors': 1
-    }, function (data) {
-      lint.assert.equal(36, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-element-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-element-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(30, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-attribute-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-attribute-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(33, data.warningCount);
-      done();
-    });
-  });
-
-  it('[force-pseudo-nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-pseudo-nesting': false
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(28, data.warningCount);
-      done();
-    });
-  });
-
-  it('[disable all nesting: false]', function (done) {
-    lint.test(file, {
-      'no-mergeable-selectors': [
-        1,
-        {
-          'force-element-nesting': false,
-          'force-attribute-nesting': false,
-          'force-pseudo-nesting': false
-        }
-      ]
     }, function (data) {
       lint.assert.equal(19, data.warningCount);
       done();
@@ -170,8 +55,9 @@ describe('no mergeable selectors - sass', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(35, data.warningCount);
+      lint.assert.equal(18, data.warningCount);
       done();
     });
   });
+
 });
