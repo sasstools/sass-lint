bfe8b8a5e800c1dff251f630c0f13367ae916cd5
Ben Rothman <bensrothman@gmail.com>
2015-10-06 21:50:46 -0500

    :mag: Add variable-name-format rule and accompanying tests

---
+++ tests/rules/variable-name-format.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var lint = require('./_lint');
+
+var file = lint.file('variable-name-format.scss');
+
+describe('variable name format', function () {
+  //////////////////////////////
+  // Variable Name Format - Hyphenated Lowercase
+  //////////////////////////////
+  it('[convention: hyphenatedlowercase]', function (done) {
+    lint.test(file, {
+      'variable-name-format': 1
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Variable Name Format - Camel Case
+  //////////////////////////////
+  it('[convention: camelcase]', function (done) {
+    lint.test(file, {
+      'variable-name-format': [
+        1,
+        {
+          'convention': 'camelcase'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Variable Name Format - Snake Case
+  //////////////////////////////
+  it('[convention: snakecase]', function (done) {
+    lint.test(file, {
+      'variable-name-format': [
+        1,
+        {
+          'convention': 'snakecase'
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+
+  //////////////////////////////
+  // Variable Name Format - Allow Leading Underscore (False)
+  //////////////////////////////
+  it('[convention: allow-leading-underscore false]', function (done) {
+    lint.test(file, {
+      'variable-name-format': [
+        1,
+        {
+          'allow-leading-underscore': false
+        }
+      ]
+    }, function (data) {
+      lint.assert.equal(8, data.warningCount);
+      done();
+    });
+  });
+});
