3a7d8188df4b3c9df25903267c83d08fc4bff6ca
Dan Purdy <danjpurdy@gmail.com>
2015-10-09 01:04:39 +0100

    :white_check_mark: Add force pseudo nesting rule tests

---
+++ tests/rules/force-pseudo-nesting.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var lint = require('./_lint');
+
+//////////////////////////////
+// SCSS syntax tests
+//////////////////////////////
+describe('force pseudo nesting - scss', function () {
+  var file = lint.file('force-pseudo-nesting.scss');
+
+  it('[default]', function (done) {
+    lint.test(file, {
+      'force-pseudo-nesting': 1
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+});
+
+//////////////////////////////
+// Sass syntax tests
+//////////////////////////////
+describe('force pseudo nesting - sass', function () {
+  var file = lint.file('force-pseudo-nesting.sass');
+
+  it('[convention: 0]', function (done) {
+    lint.test(file, {
+      'force-pseudo-nesting': 1
+    }, function (data) {
+      lint.assert.equal(7, data.warningCount);
+      done();
+    });
+  });
+});
