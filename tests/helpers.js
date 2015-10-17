44b07a79bf324141e08d8187a9ab8338871d827c
Ben Griffith <gt11687@gmail.com>
2015-10-17 14:44:19 +0100

    :white_check_mark: Add tests for isNestable helper rule

--- tests/helpers.js
+++ tests/helpers.js
@@ -888,4 +888,31 @@ describe('helpers', function () {
     done();
   });
 
+  //////////////////////////////
+  // isNestable
+  //////////////////////////////
+
+  it('isNestable - nest attribute in selector', function (done) {
+    var elements = ['selector', 'class', 'id', 'attribute'],
+        nestable = ['class', 'selector', 'attribute'],
+        previous = 'selector',
+        current = 'attribute';
+
+    var result = helpers.isNestable(current, previous, elements, nestable);
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isNestable - nest id in class', function (done) {
+    var elements = ['selector', 'class', 'id', 'attribute'],
+        nestable = ['class', 'selector', 'attribute'],
+        previous = 'class',
+        current = 'id';
+
+    var result = helpers.isNestable(current, previous, elements, nestable);
+
+    assert.equal(false, result);
+    done();
+  });
 });
