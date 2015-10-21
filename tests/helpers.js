19a955f4022625b96c5182630a20a9be79ea393f
Ben Rothman <bensrothman@gmail.com>
2015-10-21 03:28:28 -0500

    :white_check_mark: Add unit tests for helper functions

--- tests/helpers.js
+++ tests/helpers.js
@@ -892,6 +892,198 @@ describe('helpers', function () {
   });
 
   //////////////////////////////
+  // isHyphenatedBEM
+  //////////////////////////////
+
+  it('isHyphenatedBEM - [\'TEST\' - false]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('TEST');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'test\' - true]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('test');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [abcDEF - false]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('abcDEF');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'abc---def\' - false]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('abc---def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'abc___def\' - false]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('abc___def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'ab__cd__ef\' - true]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('ab__cd__ef');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'ab__cd--ef\' - true]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('ab__cd--ef');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'abc_def\' - false]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('abc_def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'abc-def\' - true]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('abc-def');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isHyphenatedBEM - [\'ab-cd__ef\' - true]', function (done) {
+
+    var result = helpers.isHyphenatedBEM('ab-cd__ef');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  //////////////////////////////
+  // isStrictBEM
+  //////////////////////////////
+
+  it('isStrictBEM - [\'TEST\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('TEST');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'test\' - true]', function (done) {
+
+    var result = helpers.isStrictBEM('test');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isStrictBEM - [abcDEF - false]', function (done) {
+
+    var result = helpers.isStrictBEM('abcDEF');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'abc---def\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('abc---def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'abc___def\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('abc___def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab__cd__ef\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('ab__cd__ef');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab__cd--ef\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('ab__cd--ef');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab__cd_ef_gh\' - true]', function (done) {
+
+    var result = helpers.isStrictBEM('ab__cd_ef_gh');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab-cd-ef__gh-ij-kl_mn-op-qr_st-uv-wx\' - true]', function (done) {
+
+    var result = helpers.isStrictBEM('ab-cd-ef__gh-ij-kl_mn-op-qr_st-uv-wx');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab__cd_ef_gh_ij\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('ab__cd_ef_gh_ij');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'abc_def\' - false]', function (done) {
+
+    var result = helpers.isStrictBEM('abc_def');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'abc-def\' - true]', function (done) {
+
+    var result = helpers.isStrictBEM('abc-def');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isStrictBEM - [\'ab-cd__ef\' - true]', function (done) {
+
+    var result = helpers.isStrictBEM('ab-cd__ef');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  //////////////////////////////
   // isValidHex
   //////////////////////////////
 
