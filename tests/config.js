d19e68011c7726225967a7926302d0e2c090d348
Dan Purdy <danjpurdy@gmail.com>
2015-11-14 00:50:13 +0000

    :green_heart: Fix config assertions

--- tests/config.js
+++ tests/config.js
@@ -1,102 +1,120 @@
 'use strict';
 
 var equal = require('deep-equal'),
+    assert = require('assert'),
     yaml = require('js-yaml'),
     fs = require('fs'),
     path = require('path'),
     merge = require('merge'),
     config = require('../lib/config');
 
-var defaults = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'lib', 'config', 'sass-lint.yml'), 'utf8'));
-
-var options = {
-  'options': {
-    'formatter': 'stylish'
-  },
-  'files': {
-    'ignore': [
-      'foo',
-      'bar'
-    ]
-  },
-  'rules': {
-    'no-duplicate-property': 0,
-    'indentation': [
-      2,
-        {
-          'size': 4
-        }
-    ]
-  }
+var custOptions = function () {
+  return {
+    'options': {
+      'formatter': 'stylish',
+      'cache-config': false
+    },
+    'files': {
+      'ignore': [
+        'foo',
+        'bar'
+      ]
+    },
+    'rules': {
+      'no-duplicate-property': 0,
+      'indentation': [
+        2,
+          {
+            'size': 4
+          }
+      ]
+    }
+  };
 };
 
 describe('config', function () {
   it('should return the defaults if no config is passed in', function (done) {
-    var conf = config();
+    var defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'lib', 'config', 'sass-lint.yml'), 'utf8')),
+        conf = config();
 
-    equal(
-      conf,
-      defaults,
-      {
-        'strict': true
-      }
+    assert(
+      equal(
+        conf,
+        defaultConfig,
+        {
+          'strict': true
+        }
+      )
     );
 
     done();
   });
 
   it('should merge options when passed in', function (done) {
-    var conf = config(options),
-        merged = merge.recursive(defaults, options);
 
-    equal(
-      conf,
-      merged,
-      {
-        'strict': true
-      }
+    var defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'lib', 'config', 'sass-lint.yml'), 'utf8')),
+        tempOptions = custOptions(),
+        conf = config(tempOptions),
+        merged = merge.recursive(defaultConfig, tempOptions);
+
+    assert(
+      equal(
+        conf,
+        merged,
+        {
+          'strict': true
+        }
+      )
     );
 
     done();
   });
 
   it('should use a config file if one is provided', function (done) {
-    var conf = config({
+    var defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'lib', 'config', 'sass-lint.yml'), 'utf8')),
+        tempOptions = {
           'options': {
-            'config-file': 'tests/yml/.stylish-output.yml'
+            'config-file': 'tests/yml/.stylish-output.yml',
+            'cache-config': false
           }
-        }),
-        merged = merge.recursive(defaults, fs.readFileSync(path.join(__dirname, 'yml', '.stylish-output.yml')));
+        },
+        conf = config(tempOptions),
+        merged = merge.recursive(tempOptions, defaultConfig, yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'yml', '.stylish-output.yml'), 'utf8')));
 
-    equal(
-      conf,
-      merged,
-      {
-        'strict': true
-      }
+    assert(
+      equal(
+        conf,
+        merged,
+        {
+          'strict': true
+        }
+      )
     );
 
     done();
   });
 
   it('should not merge rules when `merge-default-rules` is false', function (done) {
-    var opts = options,
+    var defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'lib', 'config', 'sass-lint.yml'), 'utf8')),
+        tempOptions = custOptions(),
         conf,
         merged;
 
-    opts.options['merge-default-rules'] = false;
+    tempOptions.options['merge-default-rules'] = false;
 
-    merged = merge.recursive(defaults, opts);
-    merged.rules = opts.rules;
+    conf = config(tempOptions);
 
-    conf = config(opts);
+    merged = merge.recursive(tempOptions, defaultConfig);
+    merged.rules = tempOptions.rules;
 
-    equal(
-      conf,
-      merged,
-      {
-        'strict': true
-      }
+    assert(
+      equal(
+        conf,
+        merged,
+        {
+          'strict': true
+        }
+      )
     );
 
     done();
