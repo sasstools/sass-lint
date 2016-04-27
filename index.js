150ac260a7efa5ce1e271249f6b9abcf3f9bb7b7
Michael Sive <mikesive@gmail.com>
2016-04-28 09:47:28 +1000

    return empty json if no results

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ sassLint.format = function (results, options, configPath) {
 sassLint.outputResults = function (results, options, configPath) {
   var config = this.getConfig(options, configPath);
 
-  if (this.resultCount(results)) {
+  // if (this.resultCount(results)) {
 
     var formatted = this.format(results, options, configPath);
 
@@ -231,7 +231,7 @@ sassLint.outputResults = function (results, options, configPath) {
     else {
       console.log(formatted);
     }
-  }
+  // }
   return results;
 };
 
