f836e8c43f6e7bad9a2081ff2e0639b8e7805695
Dan Purdy <dan@loftdigital.com>
2016-05-06 17:08:36 +0100

    :white_check_mark: Add pascal helper tests

--- tests/helpers.js
+++ tests/helpers.js
@@ -459,6 +459,50 @@ describe('helpers', function () {
   });
 
   //////////////////////////////
+  // isPascalCase
+  //////////////////////////////
+
+  it('isPascalCase - [\'TEST\' - false]', function (done) {
+
+    var result = helpers.isPascalCase('TEST');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isPascalCase - [\'test\' - false]', function (done) {
+
+    var result = helpers.isPascalCase('test');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isPascalCase - [AbcDEF - true]', function (done) {
+
+    var result = helpers.isPascalCase('AbcDEF');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  it('isPascalCase - [\'123\' - false]', function (done) {
+
+    var result = helpers.isPascalCase('123');
+
+    assert.equal(false, result);
+    done();
+  });
+
+  it('isPascalCase - [\'ABcDeF\' - true]', function (done) {
+
+    var result = helpers.isPascalCase('ABcDeF');
+
+    assert.equal(true, result);
+    done();
+  });
+
+  //////////////////////////////
   // isHyphenatedLowercase
   //////////////////////////////
 
