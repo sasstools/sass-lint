1ec22b170e9bda05acec924f1a5912472a408f32
JordanDDisch <jordanddisch@gmail.com>
2016-09-05 16:50:11 -0600

    852: Enhanced trailing semicolon to lint double semicolons

--- tests/rules/trailing-semicolon.js
+++ tests/rules/trailing-semicolon.js
@@ -12,7 +12,7 @@ describe('trailing semicolon - scss', function () {
     lint.test(file, {
       'trailing-semicolon': 1
     }, function (data) {
-      lint.assert.equal(5, data.warningCount);
+      lint.assert.equal(8, data.warningCount);
       done();
     });
   });
@@ -26,7 +26,7 @@ describe('trailing semicolon - scss', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(1, data.warningCount);
+      lint.assert.equal(4, data.warningCount);
       done();
     });
   });
