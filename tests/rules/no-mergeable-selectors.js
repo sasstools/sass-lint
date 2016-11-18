76287bc2d2fbf8d0b07f980aaae211336c14fe25
Ben Griffith <gt11687@gmail.com>
2016-11-18 00:15:33 +0000

    :art: Use and fix issues related to gonzales-pe v4.1.1

--- tests/rules/no-mergeable-selectors.js
+++ tests/rules/no-mergeable-selectors.js
@@ -32,7 +32,6 @@ describe('no mergeable selectors - scss', function () {
 
 });
 
-// 1 less warning than scss syntax as we dont attempt to merge media queries
 describe('no mergeable selectors - sass', function () {
   var file = lint.file('no-mergeable-selectors.sass');
 
@@ -40,12 +39,11 @@ describe('no mergeable selectors - sass', function () {
     lint.test(file, {
       'no-mergeable-selectors': 1
     }, function (data) {
-      lint.assert.equal(21, data.warningCount);
+      lint.assert.equal(22, data.warningCount);
       done();
     });
   });
 
-  // 1 less warning than scss syntax as we dont attempt to merge media queries
   it('[whitelist: div p]', function (done) {
     lint.test(file, {
       'no-mergeable-selectors': [
@@ -57,7 +55,7 @@ describe('no mergeable selectors - sass', function () {
         }
       ]
     }, function (data) {
-      lint.assert.equal(20, data.warningCount);
+      lint.assert.equal(21, data.warningCount);
       done();
     });
   });
