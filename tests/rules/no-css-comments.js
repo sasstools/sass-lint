e1f472460fe42c06b5ccb7cfa67d46d22633af37
Seena <seenarowhani95@gmail.com>
2017-01-15 21:12:17 -0500

    Added tests for no-css-comments

--- tests/rules/no-css-comments.js
+++ tests/rules/no-css-comments.js
@@ -2,34 +2,44 @@
 
 var lint = require('./_lint');
 
-//////////////////////////////
-// SCSS syntax tests
-//////////////////////////////
-describe('no css comments - scss', function () {
-  var file = lint.file('no-css-comments.scss');
-
-  it('enforce', function (done) {
-    lint.test(file, {
-      'no-css-comments': 1
-    }, function (data) {
-      lint.assert.equal(4, data.warningCount);
-      done();
+describe('no css comments', function () {
+  var config = {
+    'no-css-comments': 1
+  };
+  //////////////////////////////
+  // SCSS syntax tests
+  //////////////////////////////
+  describe('scss', function () {
+    var file = lint.file('no-css-comments.scss');
+    it('enforce', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(4, data.warningCount);
+        done();
+      });
+    });
+    it('corrects', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
-});
-
-//////////////////////////////
-// Sass syntax tests
-//////////////////////////////
-describe('no css comments - sass', function () {
-  var file = lint.file('no-css-comments.sass');
-
-  it('enforce', function (done) {
-    lint.test(file, {
-      'no-css-comments': 1
-    }, function (data) {
-      lint.assert.equal(4, data.warningCount);
-      done();
+  //////////////////////////////
+  // Sass syntax tests
+  //////////////////////////////
+  describe('sass', function () {
+    var file = lint.file('no-css-comments.sass');
+    it('enforce', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(4, data.warningCount);
+        done();
+      });
+    });
+    it('corrects', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
 });
