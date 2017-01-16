8a886ad803266404e5ac1b0d8c3400dea5f791ed
Seena <seenarowhani95@gmail.com>
2017-01-15 23:17:14 -0500

    Added tests for border-zero

--- tests/rules/border-zero.js
+++ tests/rules/border-zero.js
@@ -8,55 +8,89 @@ var lint = require('./_lint');
 describe('border zero - scss', function () {
   var file = lint.file('border-zero.scss');
 
-  it('[convention: 0]', function (done) {
-    lint.test(file, {
+  describe('[convention: 0]', function () {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 0
         }
       ]
-    }, function (data) {
-      lint.assert.equal(3, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(3, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
-
-  it('[convention: \'0\']', function (done) {
-    lint.test(file, {
+  describe('[convention: \'0\']', function () {
+    var config = {
       'border-zero': 1
-    }, function (data) {
-      lint.assert.equal(3, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(3, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
-
-  it('[convention: \'none\']', function (done) {
-    lint.test(file, {
+  describe('[convention: \'none\']', function () {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 'none'
         }
       ]
-    }, function (data) {
-      lint.assert.equal(2, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(2, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
 
-  it('invalid convention [convention: \'zero\']', function (done) {
+  describe('invalid convention [convention: \'zero\']', function () {
     // defaults to convention 0
-    lint.test(file, {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 'zero'
         }
       ]
-    }, function (data) {
-      lint.assert.equal(4, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(4, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
 });
@@ -67,55 +101,93 @@ describe('border zero - scss', function () {
 describe('border zero - sass', function () {
   var file = lint.file('border-zero.sass');
 
-  it('[convention: 0]', function (done) {
-    lint.test(file, {
+  describe('[convention: 0]', function () {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 0
         }
       ]
-    }, function (data) {
-      lint.assert.equal(3, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(3, data.warningCount);
+        done();
+      });
     });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
+    });
+
   });
 
-  it('[convention: \'0\']', function (done) {
-    lint.test(file, {
+  describe('[convention: \'0\']', function () {
+    var config = {
       'border-zero': 1
-    }, function (data) {
-      lint.assert.equal(3, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(3, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
 
-  it('[convention: \'none\']', function (done) {
-    lint.test(file, {
+  describe('[convention: \'none\']', function () {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 'none'
         }
       ]
-    }, function (data) {
-      lint.assert.equal(2, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(2, data.warningCount);
+        done();
+      });
     });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
+    });
+
   });
 
-  it('invalid convention [convention: \'zero\']', function (done) {
+  describe('invalid convention [convention: \'zero\']', function () {
     // defaults to convention 0
-    lint.test(file, {
+    var config = {
       'border-zero': [
         1,
         {
           'convention': 'zero'
         }
       ]
-    }, function (data) {
-      lint.assert.equal(4, data.warningCount);
-      done();
+    };
+    it('detects faults', function (done) {
+      lint.test(file, config, function (data) {
+        lint.assert.equal(4, data.warningCount);
+        done();
+      });
+    });
+    it('corrects faults', function (done) {
+      lint.fix(file, config, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
   });
 });
